0be566d272e6efd67735688e7e96201e10d23e2e
Rupert Kiddle <rptkiddle@gmail.com>
2025-10-30 13:27:30 +0100

    update faiss api code
    faiss.index_cpu_to_all_gpus(index, ngpu=4)

--- scripts/text/get_news_negatives.py
+++ scripts/text/get_news_negatives.py
@@ -165,17 +165,13 @@ if __name__ == "__main__":
     torch.cuda.empty_cache()
 
     # OLD CODE - 
-    # index = faiss.IndexFlatIP(len(q_embed[0]))
+    index = faiss.IndexFlatIP(len(q_embed[0]))
     # co = faiss.GpuMultipleClonerOptions()
     # co.shard = True
     # co.useFloat16 = True
-    # index = faiss.index_cpu_to_all_gpus(index, co=co)
-    # index.add(np.array(d_embed).astype(np.float32))
-
-    # NEW CODE - 
-    index = faiss.IndexFlatIP(len(q_embed[0]))
-    index = faiss.index_cpu_to_all_gpus(index)
+    index = faiss.index_cpu_to_all_gpus(index, ngpu=4)
     index.add(np.array(d_embed).astype(np.float32))
+
 
     scores, indices = knn_neighbors(q_embed, index, args.batch_size, args.k)
 
